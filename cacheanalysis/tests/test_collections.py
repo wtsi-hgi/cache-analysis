f303ad28f9cbb0bee3832b918019bc86fb14a772
Josh Holland <anowlcalledjosh@gmail.com>
2016-07-08 14:55:55 +0100

    Block out tests for RecordCollection

--- cacheanalysis/tests/test_collections.py
+++ cacheanalysis/tests/test_collections.py
@@ -1,11 +1,21 @@
 import unittest
 
+from cacheanalysis.collections import RecordCollection
+
 
 class TestRecordCollection(unittest.TestCase):
     """
-    TODO
+    Unit tests for `RecordCollection`.
     """
     # TODO
+    def setUp(self):
+        pass
+
+    def test_iterate_without_records(self):
+        pass
+
+    def test_iterate_with_records(self):
+        pass
 
 
 if __name__ == "__main__":
