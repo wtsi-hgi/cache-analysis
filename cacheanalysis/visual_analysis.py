5cae58351cbd9bb4d5fefa0bac309a784df79cb7
Colin Nolan <cn580@alumni.york.ac.uk>
2016-07-08 13:19:45 +0100

    Sorting out some overlooked issues.

--- cacheanalysis/visual_analysis.py
+++ cacheanalysis/visual_analysis.py
@@ -1,33 +1,36 @@
-from abc import ABCMeta
+from abc import abstractmethod
 
-from cacheanalysis.analysis import Analysis, BlockFileAnalysis
+from cacheanalysis.analysis import Analysis
 from cacheanalysis.analysis import BlockAnalysis
 
 
-class VisualAnalysis(Analysis, metaclass=ABCMeta):
+class VisualAnalysis(Analysis):
     """
     Visualisation for the analysis of a collection of records.
     """
+    @abstractmethod
     def visualise(self):
         """
         Visualises the collection of records.
         """
 
 
-class BlockVisualAnalysis(VisualAnalysis, BlockAnalysis, metaclass=ABCMeta):
+class BlockVisualAnalysis(VisualAnalysis, BlockAnalysis):
     """
     Visualisation for the analysis of blocks that are put in a cache.
     """
+    @abstractmethod
     def visualise(self):
         """
         Visualises what happens to the blocks in the collection of records.
         """
 
 
-class BlockFileVisualAnalysis(VisualAnalysis, BlockFileAnalysis, metaclass=ABCMeta):
+class BlockFileVisualAnalysis(BlockVisualAnalysis):
     """
     Visualisation for the analysis of known blocks that are put in a cache.
     """
+    @abstractmethod
     def visualise(self):
         """
         Visualises what happens to the blocks in the collection of records, with
@@ -42,7 +45,7 @@ class MyBlockVisualAnalysis(BlockVisualAnalysis):
     # TODO
 
 
-class MyBlockFileVisualAnalysis(BlockVisualAnalysis):
+class MyBlockFileVisualAnalysis(BlockFileVisualAnalysis):
     """
     TODO
     """
