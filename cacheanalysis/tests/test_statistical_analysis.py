fc96d5b6a76d832d796505b667389262b459d147
Josh Holland <anowlcalledjosh@gmail.com>
2016-07-11 14:39:26 +0100

    Implement tests for mean_other_block_loads_between_reload

--- cacheanalysis/tests/test_statistical_analysis.py
+++ cacheanalysis/tests/test_statistical_analysis.py
@@ -7,6 +7,7 @@ from cacheanalysis.statistical_analysis import StatisticalBlockAnalysis
 
 _BLOCK_HASH_1 = "123"
 _BLOCK_HASH_2 = "456"
+_BLOCK_HASH_3 = "789"
 _TIMESTAMP = datetime(year=2000, month=1, day=1)
 _SIZE = 10
 
@@ -23,7 +24,12 @@ class TestStatisticalBlockAnalysis(unittest.TestCase):
             CacheDeleteRecord(_BLOCK_HASH_1, _TIMESTAMP + timedelta(days=2)),
             CachePutRecord(_BLOCK_HASH_1, _TIMESTAMP + timedelta(days=3), _SIZE),
             CacheAccessRecord(_BLOCK_HASH_1, _TIMESTAMP + timedelta(days=4)),
-            CacheAccessRecord(_BLOCK_HASH_1, _TIMESTAMP + timedelta(days=5))
+            CacheAccessRecord(_BLOCK_HASH_1, _TIMESTAMP + timedelta(days=5)),
+            CacheDeleteRecord(_BLOCK_HASH_1, _TIMESTAMP + timedelta(days=6)),
+            CachePutRecord(_BLOCK_HASH_3, _TIMESTAMP + timedelta(days=7), _SIZE),
+            CacheDeleteRecord(_BLOCK_HASH_3, _TIMESTAMP + timedelta(days=8)),
+            CachePutRecord(_BLOCK_HASH_3, _TIMESTAMP + timedelta(days=9), _SIZE),
+            CachePutRecord(_BLOCK_HASH_1, _TIMESTAMP + timedelta(days=10), _SIZE)
         ]
         record_collection = RecordCollection()
         for record in self.records:
@@ -47,6 +53,12 @@ class TestStatisticalBlockAnalysis(unittest.TestCase):
     def test_mean_block_accesses_whilst_loaded_when_loaded(self):
         pass
         # TODO
+
+    def test_mean_other_block_loads_between_reload_when_not_reloaded(self):
+        self.assertIsNone(self.analysis.mean_other_block_loads_between_reload(_BLOCK_HASH_2))
+
+    def test_mean_other_block_loads_between_reload_when_reloaded(self):
+        self.assertEqual(1, self.analysis.mean_other_block_loads_between_reload(_BLOCK_HASH_1))
 
     # TODO: Tests for other methods
 
