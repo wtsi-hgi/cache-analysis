a9559add859e1caf222fbd6a73f1c44c0fd265ce
Josh Holland <anowlcalledjosh@gmail.com>
2016-07-11 14:40:14 +0100

    Implement mean_other_block_loads_between_reload

--- cacheanalysis/statistical_analysis.py
+++ cacheanalysis/statistical_analysis.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from cacheanalysis.analysis import BlockAnalysis, BlockFileAnalysis
+from cacheanalysis.models import CachePutRecord
 
 
 class StatisticalBlockAnalysis(BlockAnalysis):
@@ -24,7 +25,7 @@ class StatisticalBlockAnalysis(BlockAnalysis):
         """
         # TODO
 
-    def mean_other_block_loads_between_reload(self, block_hash: str) -> Optional[int]:
+    def mean_other_block_loads_between_reload(self, block_hash: str) -> Optional[float]:
         """
         Gets the mean number of other block loads that took place between when the given block was
         deleted from the cache and then reloaded. If the given block has not been loaded into the
@@ -32,7 +33,26 @@ class StatisticalBlockAnalysis(BlockAnalysis):
         :param block_hash: the block hash
         :return: the mean number of other block loads between reloading
         """
-        # TODO
+        puts = self.record_collection.get_block_puts(block_hash)
+        deletes = self.record_collection.get_block_deletes(block_hash)
+        if len(puts) < 2 or len(deletes) == 0:
+            return None
+        # sort all records into chronological order
+        records = sorted(
+            [record for record in self.record_collection],
+            key=lambda record: record.timestamp
+        )
+        block_in_cache = True  # don't include data before the block is added for the first time
+        other_block_loads = []
+        for record in records:
+            if record in puts:
+                block_in_cache = True
+            elif record in deletes:
+                block_in_cache = False
+                other_block_loads.append(0)
+            elif type(record) == CachePutRecord and not block_in_cache:
+                other_block_loads[-1] += 1
+        return sum(other_block_loads) / len(other_block_loads)
 
     # TODO: Anything else that might be useful to know
 
