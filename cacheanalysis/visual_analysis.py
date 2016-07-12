3a5bc077b0d4d3f36b454826108fcfe5a179662d
Josh Holland <anowlcalledjosh@gmail.com>
2016-07-12 14:45:41 +0100

    Shuffle imports to adhere to PEP 8

--- cacheanalysis/visual_analysis.py
+++ cacheanalysis/visual_analysis.py
@@ -1,10 +1,10 @@
 from abc import abstractmethod
 from operator import itemgetter
 
+from tabulate import tabulate
+
 from cacheanalysis.analysis import Analysis
 from cacheanalysis.analysis import BlockAnalysis
-
-from tabulate import tabulate
 
 
 class VisualAnalysis(Analysis):
