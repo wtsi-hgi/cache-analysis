1e8ff7c948c602e25ff06fbc5a6b9ba354b86ebb
Josh Holland <anowlcalledjosh@gmail.com>
2016-07-19 14:59:16 +0100

    Implement tests for VisualBlockFileAnalysis

--- cacheanalysis/tests/test_visual_analysis.py
+++ cacheanalysis/tests/test_visual_analysis.py
@@ -1,10 +1,11 @@
 import unittest
 from datetime import datetime
+from itertools import chain
 
 from cacheusagesimulator.usage_generator import UsageGenerator
 
 from cacheanalysis.collections import RecordCollection
-from cacheanalysis.visual_analysis import VisualBlockAnalysis
+from cacheanalysis.visual_analysis import VisualBlockAnalysis, VisualBlockFileAnalysis
 
 
 _BLOCK_HASH_1 = "123"
@@ -30,6 +31,25 @@ class TestVisualBlockAnalysis(unittest.TestCase):
 
     def test_visual_analysis(self):
         self.analysis.visualise()
+
+
+class TestVisualBlockFileAnalysis(unittest.TestCase):
+    """
+    Unit tests for `VisualBlockFileAnalysis`.
+    """
+    def setUp(self):
+        self.usage_generator = UsageGenerator()
+        self.records = [self.usage_generator.generate() for _ in range(30000)]
+        print("records:", len(self.records))
+        print("average blocks read between reference reads:", self.usage_generator.average_block_reads_between_reference_read)
+        record_collection = RecordCollection()
+        for record in self.records:
+            record_collection.add_record(record)
+        self.analysis = VisualBlockFileAnalysis(record_collection)
+
+    def test_visual_analysis(self):
+        blocks_to_display = list(chain.from_iterable([f.block_hashes for f in self.usage_generator.known_reference_files]))
+        self.analysis.visualise(blocks_to_display)
 
 
 if __name__ == "__main__":
