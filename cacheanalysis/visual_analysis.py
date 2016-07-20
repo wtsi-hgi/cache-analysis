405408d7b5e1080984019ca76a1631255c0ad4b2
Josh Holland <anowlcalledjosh@gmail.com>
2016-07-20 11:50:02 +0100

    Use OOP matplotlib API instead of pyplot

--- cacheanalysis/visual_analysis.py
+++ cacheanalysis/visual_analysis.py
@@ -1,7 +1,8 @@
 from abc import abstractmethod
 from collections import Counter
-from typing import List
+from typing import Iterable, List, Sequence, Tuple
 
+import matplotlib
 from matplotlib import pyplot as plt
 from tabulate import tabulate
 
@@ -32,8 +33,20 @@ class VisualBlockAnalysis(VisualAnalysis, BlockAnalysis):
         """
         Visualises what happens to the blocks in the collection of records.
         """
-        self.misses_vs_hits(plt, self.block_hashes, self.statistical_analysis)
+        # self.plot_misses_hits(plt, self.block_hashes, self.statistical_analysis)
+        # plt.show()
+
+        fig = plt.figure()
+
+        ax = fig.add_subplot(1, 1, 1)  # 1 row, 1 column, 1st subplot
+        ax.set_title("Cache misses against cache hits")
+        x, y, size = self.get_misses_hits(self.block_hashes, self.statistical_analysis)
+        self.plot_misses_hits(ax, x, y, s=size)
+
         plt.show()
+
+        plt.close(fig)  # pyplot keeps a reference to fig unless close() is called.
+
         # print("Blocks sorted by number of accesses (hits + misses)")
         # # This shows basically how popular a block is
         # print(tabulate(
@@ -57,7 +70,7 @@ class VisualBlockAnalysis(VisualAnalysis, BlockAnalysis):
         # ))
 
     @staticmethod
-    def misses_vs_hits(plot, block_hashes, statistical_analysis):
+    def get_misses_hits(block_hashes: Iterable[str], statistical_analysis: StatisticalBlockAnalysis) -> Tuple[List[int], List[int], List[int]]:
         count = Counter()
         for block_hash in block_hashes:
             x = statistical_analysis.total_block_misses(block_hash)
@@ -66,13 +79,15 @@ class VisualBlockAnalysis(VisualAnalysis, BlockAnalysis):
         xysize = []
         for k, v in count.items():
             xysize.append((*k, v))
-        x, y, size = zip(*xysize)
-        plot.scatter(x, y, s=size, color="blue", marker="o", edgecolors="none")
-        plot.title("Cache misses against cache hits")
-        plot.xlabel("Cache misses")
-        plot.ylabel("Cache hits")
-        plot.xlim(-0.5, max(10, max(x)) + .5)
-        plot.ylim(-0.5, max(10, max(y)) + .5)
+        return zip(*xysize)
+
+    @staticmethod
+    def plot_misses_hits(ax: matplotlib.axes.Axes, x: Sequence[int], y: Sequence[int], **kwargs) -> matplotlib.collections.PathCollection:
+        ax.set_xlabel("Cache misses")
+        ax.set_ylabel("Cache hits")
+        ax.set_xlim(-0.5, max(10, max(x)) + .5)
+        ax.set_ylim(-0.5, max(10, max(y)) + .5)
+        return ax.scatter(x, y, edgecolors="none", **kwargs)
 
 
 class VisualBlockFileAnalysis(VisualBlockAnalysis, BlockFileAnalysis):
@@ -83,19 +98,29 @@ class VisualBlockFileAnalysis(VisualBlockAnalysis, BlockFileAnalysis):
         super().__init__(record_collection)
         self.statistical_analysis = StatisticalBlockFileAnalysis(record_collection)
 
-    def visualise(self, display_hashes: List[str]=None):
+    def visualise(self, display_hashes: Sequence[str]=()):
         """
         Visualises what happens to the blocks in the collection of records, with
         information on what file each block belongs to.
-        :param display_hashes: hashes of blocks to display. If None, will display all blocks.
+        :param display_hashes: hashes of blocks to display. If empty, will display all blocks.
         """
-        plt.figure(figsize=(8, 12))
+        fig = plt.figure()
 
-        plt.subplot(211)
-        self.misses_vs_hits(plt, self.block_hashes, self.statistical_analysis)
+        ax1 = fig.add_subplot(2 if display_hashes else 1, 1, 1)
+        ax1.set_title("Cache misses against cache hits")
+        x, y, size = self.get_misses_hits(self.block_hashes, self.statistical_analysis)
+        self.plot_misses_hits(ax1, x, y, s=size)
 
-        plt.subplot(212)
-        self.misses_vs_hits(plt, filter(lambda x: not display_hashes or x in display_hashes,
-                                        self.block_hashes), self.statistical_analysis)
+        if display_hashes:
+            # Double the height of the figure so that subplots do not overlap.
+            fig.set_figheight(fig.get_figheight() * 2, forward=True)
+            ax2 = fig.add_subplot(2, 1, 2)
+            ax2.set_title("Cache misses against cache hits, filtered")
+            x, y, size = self.get_misses_hits(
+                [h for h in self.block_hashes if h in display_hashes], self.statistical_analysis
+            )
+            self.plot_misses_hits(ax2, x, y, s=size)
 
         plt.show()
+
+        plt.close(fig)
